86b630fa21ddd24c4a24e08bc7d42ffaeb7670ca
代建杉 <daijianshan33@outlook.com>
2024-12-07 16:28:54 +0800

    fresh

---
+++ README.md
@@ -0,0 +1,5 @@
+# 三三文档
+
+- `pnpm install`：安装依赖
+- `pnpm docs:dev`：启动本地服务器
+- `pnpm docs:build`：构建静态页面（`src/.vuepress/dist`）
--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -1,10 +1,4 @@
 import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
-  "/",
-  {
-    text: "33OJ",
-    icon: "code",
-    link: "https://oj.33dai.cn",
-  },
 ]);
